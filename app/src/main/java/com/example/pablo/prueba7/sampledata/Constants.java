24d8468f4370edfa0a12398785160f5e0b499ba8
JoseAntonio <joseantonio.garcia.rdz@gmail.com>
2019-07-03 18:09:47 -0500

    vesion y cambio de ip

--- app/src/main/java/com/example/pablo/prueba7/sampledata/Constants.java
+++ app/src/main/java/com/example/pablo/prueba7/sampledata/Constants.java
@@ -1,7 +1,7 @@
 package com.example.pablo.prueba7.sampledata;
 
 public class Constants {
-    //public static final String NEW_URL="http://187.248.67.68:9002/SoftvWCFService.svc/";
+    //public static final String NEW_URL="http://187.163.207.252:9002/SoftvWCFService.svc/";
     //public static final String NEW_URL="http://45.226.32.2:5600/SoftvWCFService.svc/";
     public static final String NEW_URL="http://192.168.0.5:10000/SoftvWCFService.svc/";
     public static final String URL_GET_USER = "Usuario/LogOn";
