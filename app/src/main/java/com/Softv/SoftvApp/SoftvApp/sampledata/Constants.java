fc13b075a54b9b01777ead61461d91875c59968e
JoseAntonio <joseantonio.garcia.rdz@gmail.com>
2019-10-08 17:16:20 -0500

    Ip y logos

--- app/src/main/java/com/Softv/SoftvApp/SoftvApp/sampledata/Constants.java
+++ app/src/main/java/com/Softv/SoftvApp/SoftvApp/sampledata/Constants.java
@@ -2,7 +2,7 @@ package com.Softv.SoftvApp.SoftvApp.sampledata;
 
 public class Constants {
     //public static final String NEW_URL="http://192.168.0.25:10000/SoftvWCFService.svc/";
-    public static final String NEW_URL="http://187.188.52.58:5600/SoftvWCFService.svc/";
+    public static final String NEW_URL="http://201.168.23.214:5000/SoftvWCFService.svc/";
     public static final String URL_GET_USER = "Usuario/LogOn";
     public static final String URL_GET_TECNICO = "AplicacionMovil/Get_ClvTecnico";
     public static final String URL_GET_PROX = "AplicacionMovil/GetDameSiguienteCita";
